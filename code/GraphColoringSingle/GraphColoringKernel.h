915202a9fe6eadf45706e20d1b72b27c1a045eee
Severus Tseng <37227883+SeverusTseng@users.noreply.github.com>
2018-03-14 10:41:35 -0700

    Update GraphColoringKernel.h

--- code/GraphColoringSingle/GraphColoringKernel.h
+++ code/GraphColoringSingle/GraphColoringKernel.h
@@ -13,24 +13,44 @@ void WorkItemRank(int *scan, int *lbs, int *wir, int sizeLbs) {
 	}
 }
 
-/* GraphColoring,let each thread compare one host vertex's value with one of its neighbor vertexes */
+
+/* GraphColoring,let each thread compare one host vertex's value with one of its neibor vertex */
 /* if host vertex's value is smaller than its neibor, do not assign it a color */
 __global__ 
-void GraphColoringKernel(int numColor,int *col_id, int *offset, int *bls, int *wir, int *randoms, int *colors)
+void GraphColoringKernel(int numColor, uint32_t NumRow, uint32_t *col_id, uint32_t *offset, int *lbs, int *wir, int *randoms, int *colors, bool* set)
 {   
-for (int i = threadIdx.x + blockIdx.x * blockDim.x; i < V; i = i + blockDim.x * gridDim.x) 
+for (int i = threadIdx.x + blockIdx.x * blockDim.x; i < NumRow; i = i + blockDim.x * gridDim.x) 
  {  
     int host_id = lbs[i];
     int neibor_id = col_id[offset[host_id] + wir[i]];
 
-    if ((colors[host_id] != 0)) continue;
+    if ((colors[host_id] != 0)) return;
 
     int host_value = randoms[host_id];
     int neibor_value = randoms[neibor_id];
     int neibor_color = colors[neibor_id];
 
-      if (((neibor_color != 0) && (neibor_color != numColor)) || (host_id == neibor_id)) continue;
-        if (host_value < neibor_value) continue;
-         colors[host_id] = numColor;
- }
+      if (host_id == neibor_id) return;
+       if ((neibor_color != 0) && (neibor_color != numColor)) return;
+        if (host_value < neibor_value)
+          {
+            set[host_id] = false;
+          }
+  }
+}
+
+/* the setTrue is initiated to be true
+if the vertex won't be colored or has already been colored, the setTrue is false
+else the setTrue is true*/
+__global__
+void ColorChanging(int numColor, uint32_t NumRow, int* colors, bool* setTrue)
+{
+  for (int i = threadIdx.x + blockIdx.x * blockDim.x; i < NumRow; i = i + blockDim.x * gridDim.x)
+  {
+    if ((colors[i] != 0)) continue;
+    if (setTrue[i]) 
+      {
+        colors[i] = numColor;
+      }
+  }
 }
